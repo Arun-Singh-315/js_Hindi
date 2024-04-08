64f74643af0a36314d4a2b1d9a52ad055a22f3e0
Arun Singh <64498578+Arun-Singh-315@users.noreply.github.com>
2024-04-08 06:40:53 +0000

    Data Type updated

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all Js code as newer version
+
+// alert( 3 + 3) //We are using  nodejs, not browser
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => stand alone value
+// undefined 
+
+// console.log(typeof age);
+
+console.log(typeof undefined);
+console.log(typeof null);
