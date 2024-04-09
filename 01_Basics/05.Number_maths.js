9a9692b401183327ae0e3bb94d15945a85bf13a0
Arun Singh <64498578+Arun-Singh-315@users.noreply.github.com>
2024-04-09 12:16:33 +0000

    Number and Date

---
+++ 01_Basics/05.Number_maths.js
@@ -0,0 +1,13 @@
+const balance = new Number(400)
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otheNumber = 123.456
+console.log(otheNumber.toPrecision(3));
+
+//=================================================//
+
+//Maths 
+
