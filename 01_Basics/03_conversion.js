d5dd581692137a59218c8770e370a1359bf84ec6
Arun Singh <64498578+Arun-Singh-315@users.noreply.github.com>
2024-04-09 09:35:51 +0000

    Updated strict comparisions

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -9,3 +9,29 @@ console.log(typeof(valueInNumber));
 // " " => False
 // "Arun" => True
 
+// *************** Operations *************//
+
+console.log(typeof("2"+ 3))
+console.log(typeof(2 + "3"))
+console.log(typeof(2 + 3 + "4"))
+
+console.log(true)
+console.log(+true);
+console.log(+" ")
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+//Data Type should be same 
+//Type Script -> Strict rules
+
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+//Comparision operator converts null into 0
+
+// Try to avoid such comparisions//
+
+//Strict Comparisions ===
+
