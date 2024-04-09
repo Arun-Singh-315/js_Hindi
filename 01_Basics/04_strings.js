1b8cebd07edc5dc1508158bb4075763cf83cc802
Arun Singh <64498578+Arun-Singh-315@users.noreply.github.com>
2024-04-09 11:13:14 +0000

    String

---
+++ 01_Basics/04_strings.js
@@ -0,0 +1,6 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "vs"); --> Not to be used Use 
+
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
